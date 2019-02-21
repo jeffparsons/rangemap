73b539f666b5ad64cc3070dbb0bacbb32e77949d
Jeff Parsons <jeff@parsons.io>
2019-02-21 21:51:38 +1100

    Add top-level crate documentation
    It's just links to the main structs, and a brief description
    of what each does.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,11 @@
+/*!
+[RangeMap](struct.RangeMap.html) is a map data structure whose keys are
+stored as ranges. Contiguous and overlapping ranges
+that map to the same value are coalesced into a single range.
+
+A corresponding [RangeSet](struct.RangeSet.html) structure is also provided.
+*/
+
 mod map;
 mod set;
 mod std_ext;
