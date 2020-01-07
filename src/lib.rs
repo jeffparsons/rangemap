2fe402f12950b12d13bf4422d2f6dd6590b0411a
Jeff Parsons <jeff@parsons.io>
2020-01-07 21:59:34 +1100

    Remove needless main in doctest

--- src/lib.rs
+++ src/lib.rs
@@ -13,40 +13,38 @@ use chrono::offset::TimeZone;
 use chrono::{Duration, Utc};
 use rangemap::RangeMap;
 
-fn main() {
-    let people = ["Alice", "Bob", "Carol"];
-    let mut roster = RangeMap::new();
+let people = ["Alice", "Bob", "Carol"];
+let mut roster = RangeMap::new();
 
-    // Set up initial roster.
-    let start_of_roster = Utc.ymd(2019, 1, 7);
-    let mut week_start = start_of_roster;
-    for _ in 0..3 {
-        for person in &people {
-            let next_week = week_start + Duration::weeks(1);
-            roster.insert(week_start..next_week, person);
-            week_start = next_week;
-        }
+// Set up initial roster.
+let start_of_roster = Utc.ymd(2019, 1, 7);
+let mut week_start = start_of_roster;
+for _ in 0..3 {
+    for person in &people {
+        let next_week = week_start + Duration::weeks(1);
+        roster.insert(week_start..next_week, person);
+        week_start = next_week;
     }
-
-    // Bob is covering Alice's second shift (the fourth shift overall).
-    let fourth_shift_start = start_of_roster + Duration::weeks(3);
-    let fourth_shift_end = fourth_shift_start + Duration::weeks(1);
-    roster.insert(fourth_shift_start..fourth_shift_end, &"Bob");
-
-    for (range, person) in roster.iter() {
-        println!("{} ({}): {}", range.start, range.end - range.start, person);
-    }
-
-    // Output:
-    // 2019-01-07UTC (P7D): Alice
-    // 2019-01-14UTC (P7D): Bob
-    // 2019-01-21UTC (P7D): Carol
-    // 2019-01-28UTC (P14D): Bob
-    // 2019-02-11UTC (P7D): Carol
-    // 2019-02-18UTC (P7D): Alice
-    // 2019-02-25UTC (P7D): Bob
-    // 2019-03-04UTC (P7D): Carol
 }
+
+// Bob is covering Alice's second shift (the fourth shift overall).
+let fourth_shift_start = start_of_roster + Duration::weeks(3);
+let fourth_shift_end = fourth_shift_start + Duration::weeks(1);
+roster.insert(fourth_shift_start..fourth_shift_end, &"Bob");
+
+for (range, person) in roster.iter() {
+    println!("{} ({}): {}", range.start, range.end - range.start, person);
+}
+
+// Output:
+// 2019-01-07UTC (P7D): Alice
+// 2019-01-14UTC (P7D): Bob
+// 2019-01-21UTC (P7D): Carol
+// 2019-01-28UTC (P14D): Bob
+// 2019-02-11UTC (P7D): Carol
+// 2019-02-18UTC (P7D): Alice
+// 2019-02-25UTC (P7D): Bob
+// 2019-03-04UTC (P7D): Carol
 ```
 
 */
