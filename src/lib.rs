c2ee7ebc84873c773ebbe100a0b59695a134eeb8
Jeff Parsons <jeff@parsons.io>
2020-07-15 08:27:25 +1000

    Explain consequences of different kinds of ranges

--- src/lib.rs
+++ src/lib.rs
@@ -5,10 +5,43 @@ value are coalesced into a single range.
 
 Corresponding [`RangeSet`] and [`RangeInclusiveSet`] structures are also provided.
 
-[`RangeMap`]: crate::RangeMap
-[`RangeInclusiveMap`]: crate::RangeInclusiveMap
-[`RangeSet`]: crate::RangeSet
-[`RangeInclusiveSet`]: crate::RangeInclusiveSet
+
+# Different kinds of ranges
+
+`RangeMap` and `RangeInclusiveMap` correspond to the [`Range`]
+and [`RangeInclusive`] types from the standard library respectively.
+For some applications the choice of range type may be obvious,
+or even be dictated by pre-existing design decisions. For other applications
+the choice may seem arbitrary, and be guided instead by convenience or
+aesthetic preference.
+
+If the choice is not obvious in your case, consider these differences:
+
+- If your key type `K` represents points on a continuum (e.g. `f64`),
+  and the choice of which of two adjacent ranges "owns" the value
+  where they touch is largely arbitrary, then it may be more natural
+  to work with half-open `Range`s like `0.0..1.0` and `1.0..2.0`. If you
+  were to use closed `RangeInclusive`s here instead, then to represent two such adjacent
+  ranges you would need to subtract some infinitesimal (which may depend,
+  as it does in the case of `f64`, on the specific value of `K`)
+  from the end of the earlier range. (See the last point below for more
+  on this problem.)
+- If you need to be able to represent ranges that _include_ every
+  point in the key domain (e.g. every value of `u8`) then you will
+  probably want to use `RangeInclusive`s like `128u8..=255u8`. Sometimes
+  it may be possible to instead work around this by using a wider key
+  type than the values you are actually trying to represent (`K=u16`
+  even though you are only trying to represent ranges covering `u8`)
+  but in these cases the key domain often represents discrete objects
+  rather than points on a continuum, and so `RangeInclusive` may
+  be a more natural way to express these ranges anyway.
+- If you are using `RangeInclusive`, then it must be possible to define
+  _successor_ and _predecessor_ functions for your key type `K`,
+  because adjacent ranges can not be detected (and thereby coalesced)
+  simply by testing their ends for equality. For key types that represent
+  points on a continuum, defining these functions may be awkward and error-prone.
+  For key types that represent discrete objects, this is usually much
+  more straightforward.
 
 
 # Example: use with Chrono
@@ -51,6 +84,14 @@ for (range, person) in roster.iter() {
 // 2019-02-25UTC (P7D): Bob
 // 2019-03-04UTC (P7D): Carol
 ```
+
+
+[`RangeMap`]: crate::RangeMap
+[`RangeInclusiveMap`]: crate::RangeInclusiveMap
+[`RangeSet`]: crate::RangeSet
+[`RangeInclusiveSet`]: crate::RangeInclusiveSet
+[`Range`]: std::ops::Range
+[`RangeInclusive`]: std::ops::RangeInclusive
 
 */
 
