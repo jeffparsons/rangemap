3d0b7dba224cfd0f85feb3eb2d18babe84648e08
Jeff Parsons <jeff@parsons.io>
2020-09-24 18:01:15 +1000

    Remove Skeptic
    It's not compatible with new versions of rustc,
    because it uses internal implementation details
    of where Cargo puts build artifacts.
    
    I've replaced it with a thing that works on nightly
    without any external tools. :)

--- build.rs
+++
@@ -1,8 +0,0 @@
-#[cfg(feature = "skeptic")]
-extern crate skeptic;
-
-fn main() {
-    // Generate doc tests for `README.md`.
-    #[cfg(feature = "skeptic")]
-    skeptic::generate_doc_tests(&["README.md"]);
-}
--- pre_commit.sh
+++ pre_commit.sh
@@ -3,6 +3,8 @@
 set -ex
 
 cargo test
+# For doc tests in "README.md".
+cargo +nightly test --features nightly
 cargo run --example roster
 cargo fmt
 cargo clippy --bins --examples --tests --benches -- -D warnings
