jeffparsons/rangemap

commit 2047d38b3c2d909a6a013d7b250f9399d1e1857b

Extend "const new" functions to remaining types

src/inclusive_map.rs
@@ -94,6 +94,13 @@ where
     V: Eq + Clone,
 {
     /// Makes a new empty `RangeInclusiveMap`.
+    #[cfg(feature = "const_fn")]
+    pub const fn new() -> Self {
+        Self::new_with_step_fns()
+    }
+
+    /// Makes a new empty `RangeInclusiveMap`.
+    #[cfg(not(feature = "const_fn"))]
     pub fn new() -> Self {
         Self::new_with_step_fns()
     }

src/inclusive_set.rs
@@ -70,6 +70,13 @@ where
     T: Ord + Clone + StepLite,
 {
     /// Makes a new empty `RangeInclusiveSet`.
+    #[cfg(feature = "const_fn")]
+    pub const fn new() -> Self {
+        Self::new_with_step_fns()
+    }
+
+    /// Makes a new empty `RangeInclusiveSet`.
+    #[cfg(not(feature = "const_fn"))]
     pub fn new() -> Self {
         Self::new_with_step_fns()
     }

Cargo.toml
@@ -35,7 +35,8 @@ serde_json = "1"
 serde1 = ["serde"]
 # So we can run doc tests from "README.md".
 nightly = []
-# Enables `RangeMap::new()` and `RangeSet::new()` to be const functions.
+# Enables `RangeMap::new()`, `RangeInclusiveMap::new()`, `RangeSet::new()`,
+# and `RangeInclusiveSet::new()` to be const functions.
 # Requires a nightly compiler because `const_btree_new` is an unstable feature,
 # but is soon to be stabilized: <https://github.com/rust-lang/rust/issues/71835>
 const_fn = []

commit e240e3757f14d3f211e000622d37d1f74f76fa67

Make `const_fn` feature work again
* Commit 2047d38 accidentally broke the const fn constructors for
  `RangeMap::new()` and `RangeSet::new()`.

src/inclusive_map.rs
@@ -126,6 +126,7 @@ where
     ///
     /// See [this issue](https://github.com/rust-lang/rust/issues/42168)
     /// for details about that stabilization process.
+    #[cfg(not(feature = "const_fn"))]
     pub fn new_with_step_fns() -> Self {
         Self {
             btm: BTreeMap::new(),
@@ -133,6 +134,13 @@ where
         }
     }
 
+    #[cfg(feature = "const_fn")]
+    pub const fn new_with_step_fns() -> Self {
+        Self {
+            btm: BTreeMap::new(),
+            _phantom: PhantomData,
+        }
+    }
     /// Returns a reference to the value corresponding to the given key,
     /// if the key is covered by any range in the map.
     pub fn get(&self, key: &K) -> Option<&V> {

src/inclusive_set.rs
@@ -101,7 +101,15 @@ where
     ///
     /// See [this issue](https://github.com/rust-lang/rust/issues/42168)
     /// for details about that stabilization process.
+    #[cfg(not(feature = "const_fn"))]
     pub fn new_with_step_fns() -> Self {
+        Self {
+            rm: RangeInclusiveMap::new_with_step_fns(),
+        }
+    }
+
+    #[cfg(feature = "const_fn")]
+    pub const fn new_with_step_fns() -> Self {
         Self {
             rm: RangeInclusiveMap::new_with_step_fns(),
         }